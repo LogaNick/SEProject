561a94288cb43746e21a5887c89b41263d20bac4
vibar <victor.bares@ensta-bretagne.org>
2018-02-18 16:18:35 -0400

    AT39.1 Add Test for Go To Exercise screen button. [Burris/Bares]
    - Create the MainActivity Test class.
    - Add the test for the GoTo Exercise Button.
    - Add gradle rule for the intents testing.

---
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/MainActivityTest.java
@@ -0,0 +1,36 @@
+package ca.dal.cs.athletemonitor.athletemonitor;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Espresso Test for the Main Activity.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule(MainActivity.class);
+
+    /**
+     * Test that the button to transfer to the exercise activity works.
+     * @throws Exception
+     */
+    @Test
+    public void testGoToExerciseButton() throws Exception {
+        //Try to get the button.
+        onView(withId(R.id.goToExerciseActivityButton)).perform(click());
+        intended(hasComponent(ExerciseActivity.class.getName()));
+    }
+}
