8bc65b1c587e3253d9cf366cbd6b1903cb64ba9c
vibar <victor.bares@ensta-bretagne.org>
2018-02-18 16:40:42 -0400

    AT39.1 Make the Exercises button Go to Exercises Activity. [Burris/Bares]
    - Change the main activity test rule to the subclass intentsTestRule so that we can use Espresso intents tests.
    - Change the name of the exercise acitivity from the default to Exercises.
    - We add the Exercise button on the main activity.
    -We made the Exercise button go to the exercise activity.

--- app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/MainActivityTest.java
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/MainActivityTest.java
@@ -1,6 +1,7 @@
 package ca.dal.cs.athletemonitor.athletemonitor;
 
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -20,8 +21,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule =
-            new ActivityTestRule(MainActivity.class);
+    public IntentsTestRule<MainActivity> mActivityRule =
+            new IntentsTestRule(MainActivity.class);
 
     /**
      * Test that the button to transfer to the exercise activity works.
--- app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/MainActivity.java
+++ app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/MainActivity.java
@@ -1,7 +1,9 @@
 package ca.dal.cs.athletemonitor.athletemonitor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.goToExerciseActivityButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent exerciseActivityIntent = new Intent(MainActivity.this, ExerciseActivity.class);
+                startActivity(exerciseActivityIntent);
+            }
+        });
     }
 }
