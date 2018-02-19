b198cd82731ada210d3794cc840b7be1e3520298
Lee Lunn <lee.lunn@dal.ca>
2018-02-19 19:21:59 -0400

    [REFACTOR] Removed AccountManager unit tests since they require running emulator [LUNN]

--- app/src/test/java/ca/dal/cs/athletemonitor/athletemonitor/AccountManagerTest.java
+++ app/src/test/java/ca/dal/cs/athletemonitor/athletemonitor/AccountManagerTest.java
@@ -12,34 +12,5 @@ import static org.junit.Assert.*;
  */
 
 public class AccountManagerTest {
-     /**
-     * Test for true positive on existing user
-     *
-     * Will pass in a username that does exist in the user accounts to the validate method and
-     * assert that the return value of validate is true
-     *
-     * @throws Exception
-     */
-    @Test
-    public void userExistsTest() throws Exception {
-        User testUser = new User("testAccount3", "testPassword");
-        AccountManager.createUser(testUser);
-        assertTrue(AccountManager.userExists(testUser.getUsername()));
-
-        // TODO: Add code to delete test user
-    }
-
-    /**
-     * Test for false positive on existing user
-     *
-     * Will pass in a username that does not exist in the user accounts to the validate method
-     * and assert that the return value of validate is false
-     *
-     * @throws Exception
-     */
-    @Test
-    public void userDoesNotExistTest() throws Exception {
-
-    }
 
 }
