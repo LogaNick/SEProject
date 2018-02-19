7d0878a9e880be087dd156d9eff478b40233c6e3
Lee Lunn <lee.lunn@dal.ca>
2018-02-19 11:41:48 -0400

    [REFACTOR] Signin click success test  [LUNN/BARREYRE]

--- app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/LoginActivityTest.java
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/LoginActivityTest.java
@@ -3,20 +3,27 @@ package ca.dal.cs.athletemonitor.athletemonitor;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+//import for intended() and hasComponent()
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.ha
+
+
 
 /**
  * UI Test for Login Activity
@@ -30,6 +37,9 @@ public class LoginActivityTest {
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<LoginActivity>(
             LoginActivity.class);
+    @Rule
+    public IntentsTestRule<LoginActivity> intentsTestRule = new IntentsTestRule<>(LoginActivity.class);
+
 
     /**
      * Test username with empty string
@@ -85,9 +95,12 @@ public class LoginActivityTest {
         //perform signin button click
         onView(withId(R.id.btnSignin)).perform(click());
 
+        //test if main activity has been launched
+        intended(hasComponent(MainActivity.class.getName()));
+
         //test correct response on successful login
-        onView(withId(R.id.lblLoginResult)).check(matches(withText(R.string.loginSuccess)));
-        onView(withId(R.id.btnSignin)).check(matches(withText(R.string.loginSuccess)));
+        //onView(withId(R.id.lblLoginResult)).check(matches(withText(R.string.loginSuccess)));
+        //onView(withId(R.id.btnSignin)).check(matches(withText(R.string.loginSuccess)));
 
 
     }
