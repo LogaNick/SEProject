27bbb796c9b34dfebbffa5a36bf0f21dd85fa00f
Lee Lunn <lee.lunn@dal.ca>
2018-02-22 07:16:39 -0400

    [REFACTOR] Separated AccountManager tests from LoginActivityUITest, generic boolean listener, delete user [LUNN/BARREYRE]

--- app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/AccountManagerTest.java
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/AccountManagerTest.java
@@ -2,13 +2,19 @@ package ca.dal.cs.athletemonitor.athletemonitor;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.os.Debug;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
 
 import org.junit.Rule;
 import org.junit.Test;
 
+
+import java.util.logging.Logger;
+
+import ca.dal.cs.athletemonitor.athletemonitor.listeners.BooleanResultListener;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
@@ -28,7 +34,7 @@ import static org.junit.Assert.assertTrue;
  * UI Test for Login Activity
  */
 
-public class LoginActivityUITest {
+public class AccountManagerTest {
     private String usernameInputTestText = "username";
     private String passwordInputTestText = "password";
 
@@ -38,47 +44,14 @@ public class LoginActivityUITest {
             LoginActivity.class);
 
     /**
-     * Test username with empty string
+     * Test helper method to generate the standard testing user account
      *
-     * @throws Exception
+     * @return Pre-determined user object with known information for testing purposes
      */
-    @Test
-    public void usernameEmptyTest() throws Exception {
-        onView(withId(R.id.txtUsername)).check(matches(withText("")));
-    }
+    private User createTestUser() {
+        User user = new User("test_user", "test_password");
 
-    /**
-     * Test username textbox with input
-     *
-     * @throws Exception
-     */
-    @Test
-    public void usernameInputTextTest() throws Exception {
-        onView(withId(R.id.txtUsername)).perform(typeText(usernameInputTestText));
-        closeSoftKeyboard();
-        onView(withId(R.id.txtUsername)).check(matches(withText(usernameInputTestText)));
-    }
-
-    /**
-     * Test password with empty string
-     *
-     * @throws Exception
-     */
-    @Test
-    public void passwordEmptyTest() throws Exception {
-        onView(withId(R.id.txtPassword)).check(matches(withText("")));
-    }
-
-    /**
-     * Test username textbox with input
-     *
-     * @throws Exception
-     */
-    @Test
-    public void passwordInputTextTest() throws Exception {
-        onView(withId(R.id.txtPassword)).perform(typeText(passwordInputTestText));
-        closeSoftKeyboard();
-        onView(withId(R.id.txtPassword)).check(matches(withText(passwordInputTestText)));
+        return user;
     }
 
     /**
@@ -91,25 +64,25 @@ public class LoginActivityUITest {
      */
     @Test
     public void userExistsTest() throws Exception {
-        User testUser = new User("testAccount7", "testPassword");
-        AccountManager.createUser(testUser);
+        final User testUser = createTestUser();
+        AccountManager.createUser(createTestUser());
 
         AccountManager.userExists(testUser.getUsername(), new AccountManager.UserExistsListener() {
             @Override
             public void onUserExists(boolean result) {
                 assertTrue(result);
+
+                //delete the test user before exiting test
+                AccountManager.deleteUser(testUser, null);
             }
-
         });
-
-        // TODO: Add code to delete test user
     }
 
     /**
      * Test for false positive on existing user
      *
-     * Will pass in a username that does not exist in the user accounts to the validate method
-     * and assert that the return value of validate is false
+     * Will pass if a username that does not exist in the user accounts is passed to the userExists
+     * method.  Asserts that the return value of is false
      *
      * @throws Exception
      */
@@ -123,5 +96,47 @@ public class LoginActivityUITest {
                 assertFalse("User should not exist, but user is found.", result);
             }
         });
+    }
+
+    /**
+     * Test for deleting a user account.
+     *
+     * First checks if the test user exists in the database.  If it does not exist, it is created
+     * and then a deleteUser call is made.  If the delete operation is completed without errors
+     * the test is assumed to be passed.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void deleteUserTest() throws Exception {
+        //get the test account
+        final User testUser = createTestUser();
+
+        AccountManager.userExists(testUser.getUsername(), new AccountManager.UserExistsListener() {
+            @Override
+            public void onUserExists(boolean result) {
+                //if the user doesn't exist, create it first
+                if (result == false) {
+                    AccountManager.createUser(testUser);
+                }
+
+                //delete the user
+                AccountManager.deleteUser(testUser, assertTrueBooleanResult());
+            }
+        });
+    }
+
+    /**
+     * Creates a BooleanResultListener with default behaviour of asserting a true result as true
+     *
+     * @return Listener with assertTrue behaviour
+     */
+    private BooleanResultListener assertTrueBooleanResult() {
+        return new BooleanResultListener() {
+            @Override
+            public void onResult(boolean result) {
+                assertTrue(result);
+            }
+        };
     }
 }
