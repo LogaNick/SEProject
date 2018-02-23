db14ce257215422c1ce3638ba05d9dfdddcc446c
Lee Lunn <lee.lunn@dal.ca>
2018-02-23 08:16:40 -0400

    [REFACTOR] Combined LoginActivityTest and LoginActivityUITest test classes [LUNN/BARREYRE]

--- app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/LoginActivityUITest.java
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/LoginActivityUITest.java
@@ -1,6 +1,7 @@
 package ca.dal.cs.athletemonitor.athletemonitor;
 
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -8,10 +9,16 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * UI Test for Login Activity
@@ -21,9 +28,15 @@ public class LoginActivityUITest {
     private String usernameInputTestText = "username";
     private String passwordInputTestText = "password";
 
+    /*
+        23 Feb 2018 -- deactivated in favour of rule that allows intents to be created
+
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
             LoginActivity.class);
+    */
+    @Rule
+    public IntentsTestRule<LoginActivity> intentsTestRule = new IntentsTestRule<>(LoginActivity.class);
 
     /**
      * Test username with empty string
@@ -69,6 +82,18 @@ public class LoginActivityUITest {
         onView(withId(R.id.txtPassword)).check(matches(withText(passwordInputTestText)));
     }
 
+    /**
+     * Test successful login when sign in button is clicked
+     *
+     * @throws Exception Generic exception
+     */
+    @Test
+    public void signInSuccessTest() throws Exception {
+        //perform sign in button click
+        onView(withId(R.id.btnSignIn)).perform(click());
 
+        //test if main activity has been launched
+        intended(hasComponent(MainActivity.class.getName()));
+    }
 
 }
