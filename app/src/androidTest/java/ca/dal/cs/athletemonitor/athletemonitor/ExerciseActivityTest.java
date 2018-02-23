8e173d9ccdce1dfbb0d91fcd8148c5486f971443
vibar <victor.bares@ensta-bretagne.org>
2018-02-23 13:51:52 -0400

    AT39.2 Show existing exercises on the exercises activity [Burris/Bares]
    - Added espresso test to create a new activity and check if it's there.
    - Render exercises by iterating over the user's exercise list and putting them on the screen.
    - Exercises are on a ScrollView so there will be a scroll bar if there are too many.

--- app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/ExerciseActivityTest.java
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/ExerciseActivityTest.java
@@ -1,20 +1,25 @@
 package ca.dal.cs.athletemonitor.athletemonitor;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.LinearLayout;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -23,8 +28,13 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExerciseActivityTest {
     @Rule
-    public IntentsTestRule<MainActivity> mActivityRule =
+    public IntentsTestRule<ExerciseActivity> mActivityRule =
             new IntentsTestRule(ExerciseActivity.class);
+
+    @BeforeClass
+    public static void setupUser(){
+        new UserManager().login("user", "pass");
+    }
 
     /**
      * Test that the button to create an exercise exist.
@@ -45,5 +55,22 @@ public class ExerciseActivityTest {
         //Try to click the button.
         onView(withId(R.id.createExerciseButton)).perform(click());
         intended(hasComponent(CreateExerciseActivity.class.getName()));
+    }
+
+    @Test
+    public void testCreateAndViewExercise(){
+        // Click the create exercise button
+        onView(withId(R.id.createExerciseButton)).perform(click());
+
+        // Enter exercise details
+        onView(withId(R.id.newExerciseName)).perform(typeText("Run"));
+        onView(withId(R.id.newExerciseDescription)).perform(typeText("Run outside"));
+        onView(withId(R.id.newExerciseTime)).perform(typeText("5"), closeSoftKeyboard());
+
+        // Click submit button
+        onView(withId(R.id.newExerciseSubmitButton)).perform(click());
+
+        // Check that the new exercise is there
+        onView(withParent(withId(R.id.exerciseLinearLayout))).check(matches(withText("Run")));
     }
 }
--- app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/ExerciseActivity.java
+++ app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/ExerciseActivity.java
@@ -1,9 +1,14 @@
 package ca.dal.cs.athletemonitor.athletemonitor;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class ExerciseActivity extends AppCompatActivity {
 
@@ -12,6 +17,7 @@ public class ExerciseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
+        // Click listener for create new exercise button
         findViewById(R.id.createExerciseButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -19,5 +25,33 @@ public class ExerciseActivity extends AppCompatActivity {
                 startActivity(createExerciseActivityIntent);
             }
         });
+
+        // Get the layout to add exercises to
+        LinearLayout layout = findViewById(R.id.exerciseLinearLayout);
+
+        boolean alternateColor = false;
+
+        // Get the user's list of exercises
+        List<Exercise> exercises = UserManager.getUserInstance().getUserExercises();
+        // Iterate and add exercises to screen
+        for(Exercise exercise : exercises){
+            // Build a new TextView for this exercise
+            TextView exerciseText = new TextView(this);
+            exerciseText.setText(exercise.getName());
+            exerciseText.setTextSize(28);
+            exerciseText.setPadding(0,30,0,30);
+            if(alternateColor) exerciseText.setBackgroundColor(Color.LTGRAY);
+            exerciseText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0, 4,0,0);
+            exerciseText.setLayoutParams(params);
+
+            // Add the text view to the screen
+            layout.addView(exerciseText);
+
+            alternateColor = !alternateColor;
+        }
     }
 }
