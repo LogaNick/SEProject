e5f4f125a3078e9a35db68cedbf82a8096717544
Zachary Wilkins <znwilkins@gmail.com>
2018-02-27 16:25:11 -0400

    Link UserInfo activity to MainActivity [Wilkins]

--- app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/MainActivity.java
+++ app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/MainActivity.java
@@ -1,11 +1,14 @@
 package ca.dal.cs.athletemonitor.athletemonitor;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import static ca.dal.cs.athletemonitor.athletemonitor.UserInformationActivity.USER_ID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
                 exerciseActivityIntent.putExtra("username", extras.getString("username"));
                 startActivity(exerciseActivityIntent);
             }
+        });
+
+        findViewById(R.id.goToUserInfo).setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Intent userInfoIntent = new Intent(MainActivity.this, UserInformationActivity.class);
+                   userInfoIntent.putExtra(USER_ID, extras.getString("username"));
+                   startActivity(userInfoIntent);
+               }
         });
     }
 
