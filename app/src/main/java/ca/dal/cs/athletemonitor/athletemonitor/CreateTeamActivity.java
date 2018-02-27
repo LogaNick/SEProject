8a107ca4a4a02678d9947c84728a24701ac38c1e
Nick Burris <nickburris13@gmail.com>
2018-02-27 16:32:14 -0400

    US18 Create activity for creating teams [Burris/Bares]
    - Add new activity for creating a team
    - Add fields and submit button that don't do anything yet

---
+++ app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/CreateTeamActivity.java
@@ -0,0 +1,13 @@
+package ca.dal.cs.athletemonitor.athletemonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_team);
+    }
+}
