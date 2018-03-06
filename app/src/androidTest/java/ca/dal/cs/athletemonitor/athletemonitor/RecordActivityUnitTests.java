853fe710c036259c620de410f60657bf984a81d1
Zachary Wilkins <znwilkins@gmail.com>
2018-03-06 12:47:38 -0400

    [UI] Add skeleton tests for US30 [Crane/Wilkins]

---
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/RecordActivityUnitTests.java
@@ -0,0 +1,34 @@
+package ca.dal.cs.athletemonitor.athletemonitor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class RecordActivityUnitTests {
+
+    @Test
+    public void clickRecordStart() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void clickRecordStop() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void clickPause() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void clickResume() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void backButtonConfirmation() {
+        assertTrue(false);
+    }
+
+}
