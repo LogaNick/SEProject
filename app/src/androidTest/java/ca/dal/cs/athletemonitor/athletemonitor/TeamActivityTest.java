c229f3d279a7c4ce771942f71155071ba57dd4c8
vibar <victor.bares@ensta-bretagne.org>
2018-03-07 12:13:49 -0400

    AT17.1 Create Team Activity Test. [Bares/Lunn]
    - Create the first test for the team activity.

---
+++ app/src/androidTest/java/ca/dal/cs/athletemonitor/athletemonitor/TeamActivityTest.java
@@ -0,0 +1,26 @@
+package ca.dal.cs.athletemonitor.athletemonitor;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by vibar on 07/03/2018.
+ */
+
+public class TeamActivityTest {
+    @Rule
+    public IntentsTestRule<TeamActivity> mActivityRule =
+            new IntentsTestRule(TeamActivity.class);
+    @Test
+    public void testHasCreateButton() throws Exception {
+        //Try to get the button.
+        onView(withId(R.id.createTeamButton));
+        onView(withId(R.id.teamList));
+    }
+
+}
