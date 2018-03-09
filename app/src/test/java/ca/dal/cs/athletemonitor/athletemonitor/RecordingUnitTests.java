fdd88aca609e0a5ff5f9b984c459d6857b2eb42b
Zachary Wilkins <znwilkins@gmail.com>
2018-03-09 12:32:05 -0400

    [Unit] Add Recording tests [Crane/Wilkins]

---
+++ app/src/test/java/ca/dal/cs/athletemonitor/athletemonitor/RecordingUnitTests.java
@@ -0,0 +1,24 @@
+package ca.dal.cs.athletemonitor.athletemonitor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RecordingUnitTests {
+
+    @Test
+    public void checkRecordingTrue() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void checkRecordingFalse() {
+        assertFalse(true);
+    }
+
+    @Test
+    public void checkLocationIsCaptured() {
+        assertTrue(/* !isEmpty() */false);
+    }
+    
+}
