beb44d771fa01b72d682e67c497d6adb2cf67ce0
Zachary Wilkins <znwilkins@gmail.com>
2018-03-14 14:38:40 -0300

    [Refactor] Add UI elements and assorted bug fixes [Crane/Wilkins]

--- app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/RecordActivity.java
+++ app/src/main/java/ca/dal/cs/athletemonitor/athletemonitor/RecordActivity.java
@@ -1,5 +1,7 @@
 package ca.dal.cs.athletemonitor.athletemonitor;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
@@ -7,13 +9,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.SystemClock;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,11 +28,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.RoundCap;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -40,12 +44,19 @@ public class RecordActivity extends AppCompatActivity implements OnMapReadyCallb
     /** Minimum dist between location refresh, in metres */
     private static final float MIN_DISTANCE = 3;
     private static final int ACCESS_FINE_LOCATION = 0;
+    private static final String DATABASE_REFERENCE = "DATABASE_REFERENCE";
+    private static final String ELAPSED_TIME = "ELAPSED_TIME";
+    private static final String LOCATION_LIST = "LOCATION_LIST";
 
+    private Chronometer timer;
+    private ImageButton recordButton;
+    private ImageButton pauseButton;
+
+    long timeWhenStopped = 0L;
     private String userId;
     private boolean isRecording = false;
     private boolean isPaused = false;
-    private Chronometer timer;
-    private List<Location> locationList = new LinkedList<>();
+    private ArrayList<Location> locationList = new ArrayList<>();
     private Polyline currentRoute = null;
     private GoogleMap mMap;
     private LocationManager locationManager;
@@ -84,6 +95,38 @@ public class RecordActivity extends AppCompatActivity implements OnMapReadyCallb
         }
     };
 
+    public static class RecordSaveFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.activity_record_save_dialog);
+            builder.setPositiveButton(
+                    R.string.activity_record_yes,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Bundle bundle = getArguments();
+                            String dbRef = bundle.getString(DATABASE_REFERENCE);
+                            long time = bundle.getLong(ELAPSED_TIME);
+                            ArrayList<Location> locationList = bundle.getParcelableArrayList(LOCATION_LIST);
+                            saveToFirebase(dbRef, time, locationList);
+                        }
+                    }
+            );
+            builder.setNegativeButton(
+                    R.string.activity_record_no,
+                    new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Do nothing
+                }
+            });
+            return builder.create();
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,6 +136,8 @@ public class RecordActivity extends AppCompatActivity implements OnMapReadyCallb
                 .getMapAsync(this);
 
         timer = (Chronometer) findViewById(R.id.record_chrono);
+        recordButton = (ImageButton) findViewById(R.id.record_button);
+        pauseButton = (ImageButton) findViewById(R.id.pause_button);
 
         setupLocationFields();
         if (checkForLocPermission()) {
@@ -117,53 +162,64 @@ public class RecordActivity extends AppCompatActivity implements OnMapReadyCallb
         return latLngs;
     }
 
-    //TODO change text while paused
     public void togglePauseStatus(View v) {
         if (isRecording) {
+
             isPaused = !isPaused;
-            if (isPaused)
+            pauseButton.setImageResource(
+                    isPaused ? R.drawable.ic_record_play : R.drawable.ic_record_pause
+            );
+
+            if (isPaused) {
+                timeWhenStopped = timer.getBase() - SystemClock.elapsedRealtime();
                 timer.stop();
-            else
+            }
+            else {
+                timer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+                timeWhenStopped = 0L;
                 timer.start();
+            }
+
         }
     }
 
-    //TODO make the button change text when recording
     public void toggleRecordStatus(View v) {
-        isRecording = !isRecording;
+        isRecording = !this.isRecording;
+        recordButton.setImageResource(
+                isRecording ? R.drawable.ic_record_stop : R.drawable.ic_record_rec
+        );
 
         if (!isRecording) {
             timer.stop();
-            timer.setBase(SystemClock.elapsedRealtime());
+            long time = SystemClock.elapsedRealtime() - timer.getBase();
             isPaused = false;
-            saveToFirebase();
+            pauseButton.setVisibility(View.INVISIBLE);
+
+            RecordSaveFragment fragment = new RecordSaveFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(DATABASE_REFERENCE, getString(R.string.activity_record_firebase, userId));
+            bundle.putLong(ELAPSED_TIME, time);
+            bundle.putParcelableArrayList(LOCATION_LIST, locationList);
+            fragment.setArguments(bundle);
+            fragment.show(getSupportFragmentManager(), "SAVE_TO_FIREBASE_FRAGMENT");
         }
         else {
+            locationList = new ArrayList<>();
+            currentRoute = null;
             timer.setBase(SystemClock.elapsedRealtime());
             timer.start();
+            pauseButton.setVisibility(View.VISIBLE);
         }
     }
 
-    private void saveToFirebase() {
+    private static void saveToFirebase(String dbRef, long time, List<Location> locationList) {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference myRef
-                = db.getReference(getString(R.string.activity_record_firebase, userId));
+                = db.getReference(dbRef);
 
-        long time = SystemClock.elapsedRealtime() - timer.getBase();
         RecordedWorkout workout = new RecordedWorkout(locationList, time);
         String key = myRef.push().getKey();
-        Task<Void> task = myRef.child(key).setValue(workout);
-        task.addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task task) {
-                if (task.isSuccessful()) {
-                    //TODO GOOD
-                }
-                else {
-                    //TODO BAD
-                }
-            }
-        });
+        myRef.child(key).setValue(workout);
     }
 
     /**
@@ -192,7 +248,6 @@ public class RecordActivity extends AppCompatActivity implements OnMapReadyCallb
             locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER, REFRESH_TIME, MIN_DISTANCE, locationListener);
         } catch (SecurityException e) {
-            //TODO Ask user for permission or something???
             e.printStackTrace();
         }
     }
